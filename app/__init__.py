7f320275d9850e9c76c1f904400a1165e3067954
Luigi <rossastro2013@gmail.com>
2025-06-23 01:17:35 +0200

    frontend updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from .db import init_db
+from flask import session
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'changeme'
+    bcrypt.init_app(app)
 
     init_db()
 
